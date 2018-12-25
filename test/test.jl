bb81c46ab5cdcebe634558c456a5bf70145e83af
Qi Zhang <singularitti@outlook.com>
2018-12-25 17:26:12 -0500

    Fix 2 bugs in test.jl
    1. `cell` and `positions` not defined
    2. a typo

--- test/test.jl
+++ test/test.jl
@@ -17,10 +17,12 @@ using JuSpglib
 end
 
 @testset "Zincblende" begin
+    cell = [0 0.5 0.5; 0.5 0 0.5; 0.5 0.5 0]
+    positions = transpose([0 0 0; 0.25 0.25 0.25])
     types = [:Zn, :S]
     symops = get_symmetry(cell, positions, types)
     @test length(symops) == 24
-    symbol = get_international(cell, positi®ons, types)
+    symbol = get_international(cell, positions, types)
     @test symbol == "F-43m"
     symbol = get_schoenflies(cell, positions, types)
     @test symbol == "Td^2"
