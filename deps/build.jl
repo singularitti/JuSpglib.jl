71c5c92b82738d6a149366dfef56cb6e2f57fd7d
Qi Zhang <singularitti@outlook.com>
2018-12-27 19:17:49 -0500

    Deprecate BinDeps.jl

--- deps/build.jl
+++
@@ -1,8 +0,0 @@
-using BinDeps
-
-@BinDeps.setup
-
-libspg = library_dependency("libsymspg")
-provides(Sources, URI("https://sourceforge.net/projects/spglib/files/spglib/spglib-1.9/spglib-1.9.4.tar.gz"), libspg)
-provides(BuildProcess, Autotools(libtarget = joinpath("src", "libsymspg.la")), libspg)
-@BinDeps.install Dict(:libsymspg => :spglib)
